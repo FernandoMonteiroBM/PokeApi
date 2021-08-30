1dc178a15a96d08ff3520605ca0aba171daf9654
Fernando Monteiro <fernandomonteirobm@gmail.com>
2021-08-30 09:12:00 -0300

    Fazendo com que o botão de anterior e de proximo leve o pokemon primeiro para o ultimo e o ultimo leve ao primeiro

--- src/Components/MontaTr/MontaTela.jsx
+++ src/Components/MontaTr/MontaTela.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./MontaTela.css";
 import axios from "axios";
 
 const MontaTela = () => {
-  const [pokemon, setPokemon] = useState(0);
+  const [pokemon, setPokemon] = useState(1);
   const [pokemonData, setPokemonData] = useState([]);
   const [pokemonType, setPokemonType] = useState("");
-  const [pokemonDescricao, setPokemonDescricao]=useState([])
+  const [pokemonMoves, setPokemonMoves] = useState("");
+  const [pokemonDescricao, setPokemonDescricao] = useState([]);
 
-  function mudaPoke() {
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
+  function adicionaPoke() {
     setPokemon(pokemon + 1);
   }
   function diminuiPoke() {
@@ -28,17 +33,21 @@ const MontaTela = () => {
       const res2 = await axios.get(url2);
       toArray.push(res.data);
       toArray2.push(res2.data);
-      setPokemonDescricao(res2.data.flavor_text_entries[12].flavor_text)
+      setPokemonDescricao(res2.data.flavor_text_entries[0].flavor_text);
       setPokemonType(res.data.types[0].type.name);
+
+      setPokemonMoves(res.data.moves);
+
       setPokemonData(toArray);
     } catch (e) {
-      console.log(e);
+      console.log("missing.no");
     }
   };
 
   return (
     <div className="App">
       {pokemonData.map((data) => {
+        
         return (
           <div className="container">
             <div>
@@ -47,7 +56,22 @@ const MontaTela = () => {
                 <h2 className="tipo">Tipo: {pokemonType}</h2>
               </div>
               <div className="imagemBg">
-                <img src={data.sprites.other["official-artwork"].front_default} alt="Sprite" />
+                <div className="imagemBg-inner">
+                  <div className="imagemBg-front">
+                    <img
+                      src={data.sprites.other["official-artwork"].front_default}
+                      alt="Sprite"
+                    />
+                  </div>
+                  <div className="imagemBg-back">
+                    <p>HP: {data.stats[0].base_stat}</p>
+                    <p>ATK: {data.stats[1].base_stat}</p>
+                    <p>DEF: {data.stats[2].base_stat}</p>
+                    <p>SP ATK: {data.stats[3].base_stat}</p>
+                    <p>SP DEF: {data.stats[4].base_stat}</p>
+                    <p>SPD: {data.stats[5].base_stat}</p>
+                  </div>
+                </div>
               </div>
               <div>
                 <h2>Descrição</h2>
@@ -55,19 +79,16 @@ const MontaTela = () => {
                   <p>Peso: {Math.round(data.weight / 4.3)} lbs</p>
                   <p>Altura: {Math.round(data.height * 3.9)}</p>
                   <p>Exp: {data.base_experience}</p>
-                  <p>
-                    Habilidades: {data.abilities[0].ability.name}
-                  </p>
+                  <p>Habilidades: {data.abilities[0].ability.name}</p>
                   <p>{pokemonDescricao}</p>
                 </div>
               </div>
               <div>
                 <h2>Moveset</h2>
-                <div className="descricao">
-                  <p>{data.moves[0].move.name}</p>
-                  <p>{data.moves[1].move.name}</p>
-                  <p>{data.moves[2].move.name}</p>
-                  <p>{data.moves[3].move.name}</p>
+                <div className="moveset">
+                  {pokemonMoves.map((moves, index) => {
+                    return index < 4 &&(<p key={index}>{moves.move.name}</p>)
+                  })}
                   <p></p>
                 </div>
               </div>
@@ -76,9 +97,13 @@ const MontaTela = () => {
         );
       })}
       <form onSubmit={handleSubmit} className="buttons">
-        <button onClick={diminuiPoke} className="button">Anterior</button>
+        <button onClick={pokemon<1?setPokemon(898):diminuiPoke} className="button">
+          Anterior
+        </button>
         <p className="indice">{pokemon}</p>
-        <button onClick={mudaPoke} className="button">Proximo</button>
+        <button onClick={pokemon>898?setPokemon(1):adicionaPoke} className="button">
+          Proximo
+        </button>
       </form>
     </div>
   );
